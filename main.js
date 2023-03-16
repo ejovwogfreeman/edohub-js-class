7d573766fd648ffa2413ff9b7c13c68da9b60beb
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2023-03-16 11:42:38 +0100

    message

---
+++ main.js
@@ -0,0 +1,101 @@
+// var let const;
+
+// var
+// initialised, re-assigned and re-decleared
+
+// let
+// initialised, re-assigned and cannot be re-declared
+
+// const
+// cannot be initialised, re-assined and re-declared
+
+// var name;
+
+// name = "freeman";
+
+// name = "Sandra";
+
+// var name = "Aminat";
+// var name = "Aminat";
+
+// let name;
+// name = "freeman";
+
+// name = "Mark powells";
+
+// const name = "freeman";
+
+// // name = "gb";
+
+// primitive and reference data types
+
+// primitive
+// number, string, boolean, null and undefined
+
+// var x, y, z;
+// x = 12;
+// // x = x + 5;
+// x--;
+
+// string concatenation
+
+// var name = "Cindy";
+
+// var school = "Uniben";
+
+// var sentence = "Hello " + name + " welcome to " + school;
+
+// var sentence = `Hello ${name} welcome to ${school}`;
+
+// var x = "Hello";
+// var y = 5;
+
+// js operators
+// arithmetic operator +, -, *, /, %, ++, --
+// assignment =
+// comparism >, <, >=, <=, ==, ===, !=, !==
+// logical operators &&, ||, !
+// conditionals if, else, elseif, switch, ternary(?)
+// Type operator
+
+// let x = 3;
+
+// if (x > 3) {
+//   console.log("x is greater than 3");
+// } else if (x === 3) {
+//   console.log("x is equal to 3");
+// } else {
+//   console.log("nothing here");
+// }
+
+// let x = 2;
+
+// if (x > 3) {
+//   console.log("x is greater than 3");
+// } else if (x < 3) {
+//   console.log("x is less than 3");
+// } else if (x === 2) {
+//   console.log("x is equal to 2");
+// } else {
+//   console.log("nothing here");
+// }
+
+// let name = "lancelot";
+
+// switch (name) {
+//   case "freeman":
+//     console.log("hello freeman welcome to class");
+//     break;
+//   case "godbless":
+//     console.log("hello godbless welcome to class");
+//     break;
+//   case "lancelot":
+//     console.log("hello lancelot welcome to class");
+//     break;
+//   default:
+//     console.log("does not match");
+// }
+
+// ternary operator
+// let x = 10;
+// console.log(x === 10 ? "x is equal to 10" : "x is not equal to 10");
